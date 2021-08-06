9b00dfdfceb3e8166c241108c9a9a05d391acce9
julian certuche <julian.certuche.c@gmail.com>
2021-08-06 16:59:18 -0500

    sonarCloud config

---
+++ src/ValoresEstructura202.js
@@ -0,0 +1,29 @@
+module.exports.ValoresEstructura202 = new Object();
+
+module.exports.ValoresEstructura202.tipoRegistro = {
+    value: 2
+};
+
+module.exports.ValoresEstructura202.codigoHabilitacion = {
+    value: 12
+};
+
+module.exports.ValoresEstructura202.tipoIDentificacionUsuario = {
+    value: ['RC','TI','CE','CC','PA','MS','AS','NV']
+};
+
+module.exports.ValoresEstructura202.sexoUsuario = {
+    value: ['M','F']
+};
+
+module.exports.ValoresEstructura202.codigoPertenenciaEtnica = {
+    value: [1, 2, 3, 4, 5, 6]
+};
+
+module.exports.ValoresEstructura202.codigoOcupacion = {
+    value: 4
+};
+
+module.exports.ValoresEstructura202.codigoNivelEducativo = {
+    value: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+};
